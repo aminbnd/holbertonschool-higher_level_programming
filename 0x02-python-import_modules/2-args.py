d20b5414366e684188f960235d48402ee1304f55
aminbnd <2212@holbertonschool.com>
2020-12-11 12:20:01 +0100

    Task2 fix pep8

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    print("{} ".format(len(argv) -1), end ='')
+    print("{} ".format(len(argv) - 1), end='')
     if len(argv) == 1:
         print("arguments.")
     elif len(argv) == 2:
@@ -10,4 +10,3 @@ if __name__ == "__main__":
         print("arguments:")
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
-
