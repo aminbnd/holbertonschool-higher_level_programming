d9c3201abedeb44470b2a65c4470e65330c56ef5
aminbnd <2212@holbertonschool.com>
2021-01-21 11:45:52 +0100

    Task12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/binPython3
+def roman_to_int(roman_string):
+    if roman_string is None or type(roman_string) != str:
+        return 0
+    else:
+        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                 'C': 100, 'D': 500, 'M': 1000}
+        s = 0
+        for i in range(len(roman_string)):
+            if (i != (len(roman_string) - 1) and
+                    roman[roman_string[i]] < roman[roman_string[i + 1]]):
+                s -= roman[roman_string[i]]
+            else:
+                s += roman[roman_string[i]]
+        return s
