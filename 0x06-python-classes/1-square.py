20f786979ecec376e04884932d8411bdfedc51c2
aminbnd <2212@holbertonschool.com>
2021-01-26 20:22:07 +0100

    size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Defining a Square """
+
+
+class Square:
+    """Square class
+    Attributes: size (int): size of the squre """
+    def __init__(self, size):
+        """size initialization"""
+        self.__size = size
