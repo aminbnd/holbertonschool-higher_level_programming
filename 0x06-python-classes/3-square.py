26b3536a99974fb5d7c815b7b26f021fed332ca7
aminbnd <2212@holbertonschool.com>
2021-01-26 20:50:23 +0100

    Area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Defining a Square """
+
+
+class Square:
+    """Square class
+    Attributes: size (int): size of the squre """
+    def __init__(self, size=0):
+        """size initialization"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Calculate the area of the square"""
+        return self.__size ** 2
