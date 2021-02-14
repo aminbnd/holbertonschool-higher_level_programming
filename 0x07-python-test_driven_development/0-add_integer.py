077e7ef9f2c2f545ed6709966f1dd23a2e276761
aminbnd <2212@holbertonschool.com>
2021-02-14 17:32:11 +0100

    Addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Definition of a function returning the sum of two integers"""
+
+def add_integer(a, b=98):
+    """ Returns a + b
+    Raises:
+        TypeError: If a or b are is not integer or float
+    """
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
