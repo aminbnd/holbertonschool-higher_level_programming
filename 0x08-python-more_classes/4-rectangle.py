eabf970e33724a11adceac10d71f219e0fdf39de
aminbnd <2212@holbertonschool.com>
2021-02-15 20:18:36 +0100

    Representation

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -81,4 +81,4 @@ class Rectangle:
         Return:
             The representation of the rectangle (str)
         """
-        return ("Rectangle ({},{})".format(self.__height, self.__width))
+        return ("Rectangle ({}, {})".format(self.__width, self.__height))
