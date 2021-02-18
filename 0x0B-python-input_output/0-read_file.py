8b112de46bb565e699aeb5c07703b4293d4d1662
aminbnd <2212@holbertonschool.com>
2021-02-18 12:51:25 +0100

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+Module containing a function that reads a text file (encoding UTF-8)
+and prints it in stdout
+"""
 def read_file(filename=""):
+    """ Function that reads a text file (encoding UTF-8)
+and prints it in stdout
+"""
     with open(filename, "r", encoding="UTF-8") as f:
         print(f.read(), end="")
