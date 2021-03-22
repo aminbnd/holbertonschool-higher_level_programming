b4af10d39e631579920853ce23d16aa8e0d9a547
aminbnd <2212@holbertonschool.com>
2021-03-22 16:29:11 +0100

    Task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,13 +2,9 @@
 
 function factorial (n) {
   let result = 1;
-  if (n === 0 || n === 1) {
-    return result;
-  } else {
-    for (let i = n; i >= 1; i--) {
-      result = result * i;
-    }
-    return result;
+  for (let i = n; i >= 1; i--) {
+    result = result * i;
   }
+  return result;
 }
 console.log(factorial(parseInt(process.argv[2])));
