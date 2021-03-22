5df34c0aad19e0fae7b3e1ced9c72193c06225b9
aminbnd <2212@holbertonschool.com>
2021-03-22 14:55:27 +0100

    Task 3

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,11 +2,12 @@
 
 let i = 0;
 process.argv.forEach((val, index) => {
+  console.log(`${val}`);
   i++;
 });
-if (i < 2) {
+if (i <= 2) {
   console.log('No argument');
-} else if (i === 2) {
+} else if (i === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
