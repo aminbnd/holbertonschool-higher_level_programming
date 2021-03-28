03a1331efe4cf20137927988e220da94aa65b91a
aminbnd <2212@holbertonschool.com>
2021-03-28 17:44:37 +0000

    Task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -15,8 +15,9 @@ if __name__ == "__main__":
                           user=argv[1], passwd=argv[2],
                           db=argv[3], charset="utf8")
     cur = cnx.cursor()
+    sta = argv[4]
     cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY id ASC"
-                .format(argv[4]))
+                .format(sta))
     rows = cur.fetchall()
     for i in rows:
         if i[1] == argv[4]:
