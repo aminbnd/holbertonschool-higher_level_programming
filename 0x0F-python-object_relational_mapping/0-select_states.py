f64fe34ef1dd791c1d79ce5d29fff872495660e6
aminbnd <2212@holbertonschool.com>
2021-03-28 16:13:15 +0000

    Task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module that lists states
+"""
+import sys
+import MySQLdb
+
+
+def main():
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur= conn.cursor()
+    query = "SELECT id,name FROM states ORDER BY id ASC"
+    cur.execute(query)
+    row = cur.fetchall()
+    for i in row:
+        print(i)
+    cur.close()
+    conn.close()
+if __name__ == "__main__":
+    main()
